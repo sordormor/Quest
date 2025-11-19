215bc672870bd9a3ad1df49339ab977814910697
sordormor <s.tavalinsky@gmail.com>
2025-11-19 08:47:49 +0300

    Demo.1

--- Demo.py
+++ Demo.py
@@ -1,4 +1,26 @@
 import Pics as p
+import Logs as l
+import random as rand
 p.logo()
+
 p.skip()
-print("Вот уже уидёт 10 день с того как ты пришёл в сознание")
+
+if input()=="" or input():
+    l.prolog1()
+
+if input()=="" or input():
+    l.prolog2()
+    
+if input()=="" or input():
+    l.prolog3()
+
+if input()=="" or input():
+    l.prolog4()
+
+if input()=="" or input():
+    l.prolog5()
+
+if input()=="" or input():
+    l.prolog6()
+
+livepower
